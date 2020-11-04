f102a735c1df509b9052b5662ad134a6a6dacded
sander0806 <72571070+sander0806@users.noreply.github.com>
2020-11-04 11:52:40 +0100

    Update wildcamera met datum.py

--- wildcamera met datum.py
+++ wildcamera met datum.py
@@ -1,14 +1,15 @@
 from gpiozero import MotionSensor
-from picamera import PiCamera 
+from picamera import PiCamera
 from datetime import datetime
+import time
 
 pir = MotionSensor(4)
 camera = PiCamera()
-filename = "{0:%c}".format(datetime.now())
 
-while True:
-    pir.wait_for_motion()
-	print("Motion detected!")
-	camera.start_recording(filename)
-	pir.wait_for_no_motion()
-	camera.stop_recording() 
+while True: 
+    pir.wait_for_motion() 
+    print("Bewegin gedecteerd") 
+    timestamp = datetime.now().strftime("%c") 
+    camera.start_recording("/home/pi/Desktop/verander.dit.naar.je.eigen.map!/{}.h264".format(timestamp))
+    time.sleep(60)
+    camera.stop_recording()
